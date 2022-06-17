db7d1bc1ca9276a6c3ee43b358f012c1e4bf78d4
ray <rayking132@gmail.com>
2022-06-17 00:14:20 -0600

    Capstone draft 1 for my Automation class

---
+++ src/test/java/Testcases/Test.java
@@ -0,0 +1,48 @@
+package Testcases;
+
+import Library.SelectBrowser;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+public class Test {
+
+    WebDriver driver;
+
+    private static ExtentHtmlReporter htmlReporter;
+    private static ExtentReports extent;
+    private static ExtentTest test;
+
+    @BeforeSuite
+    public void setupReport() {
+
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/GoogleReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+        extent.setSystemInfo("Host Name", " ray.home-server.local");
+        extent.setSystemInfo("Environment", "QA");
+        extent.setSystemInfo("UserName", "Ray Gurung");
+
+        htmlReporter.config().setChartVisibilityOnOpen(true);
+        htmlReporter.config().setDocumentTitle("Google search test");
+        htmlReporter.config().setReportName("Google search report");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+        htmlReporter.config().setTheme(Theme.DARK);
+
+    }
+
+    @BeforeTest
+    public void launchBrowser() {
+        driver = SelectBrowser.StartBrowser("Chrome");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.alexandnova.com/");
+    }
+
+}
